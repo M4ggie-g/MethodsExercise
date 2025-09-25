cd30f17167572109d5aac7d2cb83909ec23e51ca
Magdalena Gomez <maggiealexia3@gmail.com>
2025-09-25 18:26:08 -0500

    completed C# story program exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace MethodsExercise
+﻿namespace MethodsExercise;
+public class Program
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+         static void MadLib()
+         { 
+             Console.WriteLine("What is your name?");
+             string username = Console.ReadLine();
+             Console.WriteLine("What is your favorite color?");
+             string favcolor = Console.ReadLine();
+             Console.WriteLine("What is your favorite animal?");
+             string favanimal = Console.ReadLine();
+             Console.WriteLine("Favorite band?");
+             string favband = Console.ReadLine();
+
+             Console.WriteLine($"One day, {username} was walking through the woods while wearing a {favcolor} t-shirt. {username} saw a {favanimal} listening to {favband}.");
+         }
+         static void Main(string[] args)
+         {
+             MadLib();
+         }
 }
+
